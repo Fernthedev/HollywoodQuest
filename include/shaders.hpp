7302ed0eda03dd454b05a5bc558b542f80213d38
Metalit <48568911+Metalit@users.noreply.github.com>
2023-08-04 15:01:35 -0500

    Fix shader

--- include/shaders.hpp
+++ include/shaders.hpp
@@ -2,13 +2,8 @@
 
 #include "shaders/gamma_convert_vs.glsl.hpp"
 #include "shaders/gamma_rgb_convert_fs.glsl.hpp"
-#include "shaders/gamma_yuv_convert_fs.glsl.hpp"
 #include "opengl_replay/Shader.hpp"
 
 static Shader shaderRGBGammaConvert() {
     return {gamma_convert_vs_glsl, gamma_rgb_convert_fs_glsl};
-}
-
-static Shader shaderYUVGammaConvert() {
-    return {gamma_convert_vs_glsl, gamma_yuv_convert_fs_glsl};
 }
